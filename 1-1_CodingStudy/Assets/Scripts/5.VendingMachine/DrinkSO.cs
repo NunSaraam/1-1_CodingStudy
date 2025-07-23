1f16a622176f2bcfc5617a0c6822cb9bb9de7a2b
NunSaraam <yjc22950037@gmail.com>
2025-07-23 15:15:18 +0900

    [Feat] 자판기 과제중

---
+++ 1-1_CodingStudy/Assets/Scripts/5.VendingMachine/DrinkSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Drink", menuName = "Vending Machine/Drink")]
+public class DrinkSO : ScriptableObject
+{
+    [Header("기본 정보")]
+    public int id;                                  //고유 번호
+    public string drinkName = "음료수";             //음료 이름
+    public Sprite drinkIcon;                        //음료 이미지
+    public int price;                               //음료 가격
+    public int stock;                               //남은 재고
+}
