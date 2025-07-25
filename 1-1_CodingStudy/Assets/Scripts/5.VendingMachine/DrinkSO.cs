3b6e1d2cbb116f4bb19019c51bd33c737dad07b8
NunSaraam <yjc22950037@gmail.com>
2025-07-25 18:19:16 +0900

    [Feat] 자판기 UI구성

--- 1-1_CodingStudy/Assets/Scripts/5.VendingMachine/DrinkSO.cs
+++ 1-1_CodingStudy/Assets/Scripts/5.VendingMachine/DrinkSO.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 public class DrinkSO : ScriptableObject
 {
     [Header("기본 정보")]
-    public int id;                                  //고유 번호
     public string drinkName = "음료수";             //음료 이름
     public Sprite drinkIcon;                        //음료 이미지
     public int price;                               //음료 가격
-    public int stock;                               //남은 재고
 
     [Header("설명")]
     [TextArea(2, 3)]
--- 1-1_CodingStudy/Assets/Scripts/5.VendingMachine/VendingMachine.cs
+++ 1-1_CodingStudy/Assets/Scripts/5.VendingMachine/VendingMachine.cs
@@ -5,8 +5,15 @@ using UnityEngine.UI;
 
 public class VendingMachine : MonoBehaviour
 {
-    [Header("음료수 선택 버튼")]
+    [Header("음료수 목록")]
+    [SerializeField] DrinkSO[] drinks;
+
+    [Header("UI")]
     public Button[] drinkSelectButtons;
+    public Button returnCoinButton;
+    public Text[] drinkInfoText;
+    public Text userInfoText;
+    public Text inputCoinCountText;
 
     [Header("기본 정보")]
     [Tooltip("초기 소지 금액")] public int initialBalance = 5000;
@@ -14,10 +21,15 @@ public class VendingMachine : MonoBehaviour
     [Header("동전 투입 시스템")]
     [Tooltip("현재 소지 금액")] public int currentBalance;
     public int[] coinValue = {100, 500, 1000};
-    public int coinInputCount;
+    public int inputCoinCount;
 
     [Header("구매 및 거스름돈")]
     DrinkSO selectedDrink;
     public bool canAfford = false;
     public int changeAmount;
+
+    void UpdateUI()
+    {
+
+    }
 }
